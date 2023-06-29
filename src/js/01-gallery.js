ab365030cf75e53e72181db584bb8d6c9c040ccd
OleksiiProkoshin <my.mail2210@gmail.com>
2023-06-29 23:00:57 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
 
+const galleryMarkup = galleryItems
+  .map(
+    ({ original, preview, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`)
+  .join("");
+
+galleryList.innerHTML = galleryMarkup;
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+    });
+  
 console.log(galleryItems);
