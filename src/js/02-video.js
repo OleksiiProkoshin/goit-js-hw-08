2661c09df65155b63384b2557ee76c9deef65c0e
OleksiiProkoshin <my.mail2210@gmail.com>
2023-07-04 16:25:31 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+// Підключення бібліотеки lodash.throttle
+import throttle from 'lodash.throttle';
 
+//Підключення плеєра
+import Player from '@vimeo/player';
+
+// Отримання посилання на плеєр
+const playerElement = document.getElementById('vimeo-player');
+
+// Ініціалізація плеєра
+const player = new Player(playerElement);
+
+// Функція, яка зберігає поточний час відтворення в локальному сховищі
+const saveCurrentTime = throttle(() => {
+    player.getCurrentTime().then((currentTime) => {
+      localStorage.setItem('videoplayer-current-time', currentTime);
+    });
+  }, 1000); // Оновлення не частіше, ніж раз на секунду
+  
+// Додавання обробника події timeupdate
+player.on('timeupdate', saveCurrentTime);
+
+// Отримання часу відтворення з локального сховища
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+// Встановлення часу відтворення з локального сховища, якщо він збережений
+if (savedTime) {
+  player.setCurrentTime(parseFloat(savedTime));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
 
+    // Функція, яка зберігає стан форми в локальне сховище
+    function saveFormState() {
+      const email = document.querySelector('input[name="email"]').value;
+      const message = document.querySelector('textarea[name="message"]').value;
+      const formData = { email, message };
+
+      localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    }
+
+    // Оновлення сховища не частіше, ніж раз на 500 мілісекунд
+    import throttle from 'lodash.throttle';
+    const saveFormStateThrottled = throttle(saveFormState, 500);
+
+    // Виклик функції saveFormStateThrottled при події input на формі
+    document.querySelector('.feedback-form').addEventListener('input', saveFormStateThrottled);
+
+    // Функція, яка завантажує стан форми з локального сховища
+    function loadFormState() {
+      const formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+      if (formData) {
+        document.querySelector('input[name="email"]').value = formData.email;
+        document.querySelector('textarea[name="message"]').value = formData.message;
+      }
+    }
+
+    // Виклик функції loadFormState під час завантаження сторінки
+    window.addEventListener('load', loadFormState);
+
+    // Очищення сховища та полів форми при сабміті форми
+    document.querySelector('.feedback-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      clearFormState();
+      console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    });
+
+    // Очищення сховища та полів форми
+    function clearFormState() {
+      localStorage.removeItem('feedback-form-state');
+      document.querySelector('input[name="email"]').value = '';
+      document.querySelector('textarea[name="message"]').value = '';
+    }
+
+   
+
+
+    
+
